69a72d68acf35ab47d79d2e6d296bd6fc6096045
kiefl <kiefl@w311.mucam.mpg.de>
2012-06-15 17:49:14 +0200

    html templating started

--- serveIcon.js
+++ serveIcon.js
@@ -40,67 +40,38 @@ var rootPath = 		outPath,
     iconNamesHTML = "";
 
 /**
- * Writes content of variable svgIcon as response
- * 
+ * Generate icon list as html (try base64 encoding)
  */
-	var iconNames = fs.readdirSync(rootPath + "/" + setupPath[2]);
-	console.log(iconNames);
-	console.log(typeof(iconNames));
-	
-	http.createServer(function (req, res) {
 
-		  if (process.env.NODE_ENV == 'DEVELOPMENT') {
-		    mu.clearCache();
-		  }
+var pngIcon = fs.readFileSync(rootPath + "/" + setupPath[2] + "/add_document_1a_Deactivated.png");
+console.log(pngIcon);
 
-		  var stream = mu.compileAndRender("index.html", {
-			  "repo": [
-			           { "name": "This" },
-			           { "name": " is just" },
-			           { "name": " a test" },
-			           ]
-			       }
-		  );
-		  util.pump(stream, res);
-		  
-		}).listen(8089);	
-	
-	
-	/**	
-	jsdom.env({
-		/**
-		 * Create template page
-		 
-		html: "<html><body></body></html>",
-		scripts: ["http://code.jquery.com/jquery-1.5.min.js"]
-		}, function (err, window) {
-		  var $ = window.jQuery;
-		  $('body').append("Iconliste");
-		  for (e in iconNames) {
-		  $('body').append("<span><img style='width:30px; height:30px' src='" + iconNames[e] + "'> " + iconNames[e] + "</span>");
-	  		};		  
-		  console.log($("body").html());
-		  var iconNamesHTML = $("body").html();
-			fs.writeFile(rootPath + "/" + setupPath[2] + "/testIcon.html", iconNamesHTML, function(err) {
-			    if(err) {
-			        console.log(err);
-			    } else {
-			        console.log("Icon list saved as HTML.");
-			    }
-			});
-	});
+function htmlPage() {
+	var data = "das ist ein Test " +
+
+"<div style='background-color: green;'> alles was in dem div steht </div>" +
+" /><p>" +
+
+pngIcon +
+
+"</p>"
+	return data
+}
+
+
+
 
 /**
  * Direct output if necessary
- * 
+*/ 
  http.createServer( function(request, response) {
  response.writeHead(200, {"Content-Type": "text/html"});
- response.write($("body").html());
+ response.write(htmlPage());
  response.end();
  console.log(http.createServer());
- }).listen(9090);
- console.log("Server running at http://localhost:9090/");
- */
+ }).listen(8089);
+ console.log("Server running at http://localhost:8089/");
+
 
 
 
