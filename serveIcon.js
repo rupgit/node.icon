9367e9e01f40637d5c6ee1bce07cbc4f0cd2c622
win <win@win-PC.fritz.box>
2012-06-15 21:51:58 +0200

    go for templating

--- serveIcon.js
+++ serveIcon.js
@@ -9,7 +9,6 @@
 var http = require('http'),
 	fs = require('fs'),
 	util = require('util'),
-	gm = require('gm'),
     url = require('url'),
     path = require('path'),
     mu = require('mu2'),
@@ -44,26 +43,30 @@ var rootPath = 		outPath,
  */
 
 var pngIcon = fs.readFileSync(rootPath + "/" + setupPath[2] + "/add_document_1a_Deactivated.png");
+
 console.log(pngIcon);
+var image = pngIcon.toString('base64');
+console.log(image);
 
 function htmlPage() {
-	var data = "das ist ein Test " +
+	var htmlDocument = "das ist ein Test " +
 
 "<div style='background-color: green;'> alles was in dem div steht </div>" +
-" /><p>" +
+"<img alt='Embedded Image' src='data:image/png;base64," + image + "' />" +
+"<img alt='Embedded Image' src='./svgIcons/add_document_2a.svg' />"
 
-pngIcon +
-
-"</p>"
-	return data
+return htmlDocument
 }
 
-
-
+fs.writeFileSync('pngOutput.html', htmlPage(), 'utf8', function (err) {
+	  if (err) throw err;
+	  console.log('It\'s saved!');
+	});
 
 /**
  * Direct output if necessary
-*/ 
+*/ 			
+
  http.createServer( function(request, response) {
  response.writeHead(200, {"Content-Type": "text/html"});
  response.write(htmlPage());
@@ -71,15 +74,5 @@ pngIcon +
  console.log(http.createServer());
  }).listen(8089);
  console.log("Server running at http://localhost:8089/");
-
-
-
-
-			
-			
-			
-			
-			
-			
 			
 			
